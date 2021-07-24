d21e48bca41e5d9bc579fb038c9a7fcd3fa193a4
benarji123 <naradabenarji123@gmail.com>
2021-07-24 12:48:23 +0530

    thies

---
+++ src/functions/Functions.java
@@ -0,0 +1,31 @@
+package functions;
+
+
+public class Functions {
+	int sum=0;
+	String name="alekh";
+	String age="27";
+	
+	public static void main(String args[]) {
+		int[] r= {1,3,5,7,9,04};
+		Functions obj=new Functions();
+		obj.method(r);
+		Alekh obj1=new Alekh();
+		obj1.methodA(obj);
+		
+	}
+	public void method(int [] temp) {
+		for(int t:temp) {
+			sum=sum+t;
+			System.out.println(sum);
+		}
+	}
+
+}
+class Alekh{
+	public void methodA( Functions g) {
+		System.out.println(g.name);
+		System.out.println(g.age);
+		
+	}
+}
---
+++ src/functions/Javapractic.java
@@ -0,0 +1,23 @@
+package functions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Javapractic {
+public static void main(String []args) {
+Pattern p = Pattern.compile("/s",Pattern.CASE_INSENSITIVE);
+Matcher m = p.matcher("nrd enrji");
+boolean b = m.find();
+if(b) {
+	System.out.println(m.group());
+}else {
+	System.out.println("not found");
+}
+
+}
+}
+
